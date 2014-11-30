18a86f3d3bb11903d4dae998044c8959b2467ff0
Nicolai Czempin <contact@czempin.de>
2014-11-30 21:42:53 +0100

    starting the cycle

---
+++ src/de/czempin/nicnac16/simulator/Decoder.java
@@ -0,0 +1,12 @@
+package de.czempin.nicnac16.simulator;
+
+import de.czempin.nicnac16.assembler.Instruction;
+
+public class Decoder {
+
+	public static Instruction decode(int opcode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/de/czempin/nicnac16/simulator/Main.java
+++ src/de/czempin/nicnac16/simulator/Main.java
@@ -1,10 +1,23 @@
 package de.czempin.nicnac16.simulator;
 
+import de.czempin.nicnac16.assembler.Instruction;
+
 public class Main {
 
 	public static void main(String[] args) {
 		boolean done = false;
+		int PC = 0;
+		int word = 0;
+		int mem[]= new int[65636];
+		int opcode;
+		Instruction instruction = null;
 		while(!done){
+			// fetch instruction
+			word = mem[PC];
+			// decode instruction
+			opcode = word / 4096; // shift right by 12 (4096 = 2´12)
+			instruction = Decoder.decode(opcode);
+			// execute instruction
 			done = true;
 		}
 				
