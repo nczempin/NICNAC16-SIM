b6df858c56de6570ceb562747c975f514f182f92
Nicolai Czempin <contact@czempin.de>
2014-11-30 21:30:15 +0100

    dummy loop

--- src/de/czempin/nicnac16/simulator/Main.java
+++ src/de/czempin/nicnac16/simulator/Main.java
@@ -3,6 +3,11 @@ package de.czempin.nicnac16.simulator;
 public class Main {
 
 	public static void main(String[] args) {
+		boolean done = false;
+		while(!done){
+			done = true;
+		}
+				
 
 	}
 
