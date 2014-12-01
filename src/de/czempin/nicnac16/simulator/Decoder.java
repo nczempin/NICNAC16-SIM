1b37f9c03f528e97013c29d97db3fce64e73d7e3
Nicolai Czempin <contact@czempin.de>
2014-12-01 12:54:32 +0100

    moved to enum

--- src/de/czempin/nicnac16/simulator/Decoder.java
+++ src/de/czempin/nicnac16/simulator/Decoder.java
@@ -1,12 +1,11 @@
 package de.czempin.nicnac16.simulator;
 
-import de.czempin.nicnac16.assembler.AddInstruction;
 import de.czempin.nicnac16.assembler.Instruction;
 
 public class Decoder {
 
 	public static Instruction decode(int opcode) {
-		return new AddInstruction();
+		return Instruction.ADD;
 	}
 
 }
