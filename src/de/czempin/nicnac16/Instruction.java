0a08f1bc5d61325a4cbc40825bfc6846c6858f1b
Nicolai Czempin <contact@czempin.de>
2014-12-06 15:49:15 +0100

    actually do the work

--- src/de/czempin/nicnac16/Instruction.java
+++ src/de/czempin/nicnac16/Instruction.java
@@ -7,8 +7,8 @@ public enum Instruction {
 			int address = word & 0x00000FFF; // extract address
 			int value = p.readMemory(address);
 
-			int newAC = 0; //TODO
-			int oldAC = 0; //TODO
+			int oldAC = p.AC;
+			int newAC = p.AC + value;
 			String formatted = String.format("ADD %h: AC (%h)<- AC(%h) + mem[%h] (%h)", address, newAC, oldAC, address, value);
 			System.out.println(formatted);
 			p.AC += value; // TODO: condition bits
