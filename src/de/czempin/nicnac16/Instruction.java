5214c9233eef8ab5ac4914c99b26b8ba32ecf76b
Nicolai Czempin <contact@czempin.de>
2014-12-06 15:40:03 +0100

    clean up the output

--- src/de/czempin/nicnac16/Instruction.java
+++ src/de/czempin/nicnac16/Instruction.java
@@ -5,10 +5,9 @@ public enum Instruction {
 		public void execute(int word, Processor p) {
 			super.execute(word, p);
 			int address = word & 0x00000FFF; // extract address
-			System.out.println(String.format("address: %h", address));
 			int value = p.readMemory(address);
-			String formatted = String.format("value: %h", value);
-			System.out.println(formatted);
+
+			System.out.println(String.format("address: %h. value: %h", address, value));
 			p.AC += value; // TODO: condition bits
 		}
 	},
