dba30377c8f082cd439199cc5b5bd35c35630e5a
Nicolai Czempin <contact@czempin.de>
2014-12-16 14:02:59 +0100

    print out some context during memory access

--- src/de/czempin/nicnac16/Processor.java
+++ src/de/czempin/nicnac16/Processor.java
@@ -6,16 +6,34 @@ public class Processor {
 	public int AC;
 
 	public int readMemory(int address) {
+		printMemory(address);
 		return mem[address];
 	}
-
 	public void writeMemory(int address, int value) {
 		mem[address] = value;
+		printMemory(address);
 	}
+
+	private void printMemory(int address) {
+		// align to 8 words
+		int start = (address / 8) * 8;
+		System.out.print(String.format("(%04x): ", start));
+		for (int i = start; i < start + 8; i++) {
+			String string = String.format("%04x", mem[i]);
+			System.out.print(string);
+			if (i == address) {
+				System.out.print("*");
+			} else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+
 
 	public void printState() {
 		// registers
-		final String formatted = String.format("PC: %h, AC: %h",PC,AC);
+		final String formatted = String.format("PC: %h, AC: %h", PC, AC);
 		System.out.println(formatted);
 	}
 
